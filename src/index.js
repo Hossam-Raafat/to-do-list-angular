cd8cd9ac3896062e6a3d821be673aea8bb3f9222
Hossam-Raafat <juggiiboy@gmail.com>
2017-05-14 22:18:43 +0200

    add api get and post from rails

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-angular
-  .module('app', []);
+var app = angular.module('todo-app', []);
+
+app.controller(
+	'todo-controller', function($scope, $http){
+		$scope.toDo = 'To-Do';
+		$scope.done = 'Done';
+		$scope.items = [];
+		$http.get('http://localhost:3000/items.json').then(
+			function(success) {
+				$scope.items = success.data;
+			},
+			function(err) {
+				console.log(err);
+			})
+
+		$scope.addItem = function(){
+			var add = {
+				body: $scope.itemForm
+			}
+			$http.post('http://localhost:3000/items.json', add).then(
+				function(success){
+					$scope.itemForm = '';
+					$scope.items.push(success.data);
+				},
+				function(err){
+					console.log(err);
+				})
+		}
+
+			
+	});
