e880a0069edce07a01a52ed0ccc2f039311bbddf
Hossam-Raafat <juggiiboy@gmail.com>
2017-05-15 17:20:59 +0200

    Add edit feature

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ app.controller(
 		$scope.toDo = 'To-Do';
 		$scope.done = 'Done';
 		$scope.items = [];
+		$scope.editing = false;
 		$http.get('http://localhost:3000/items.json').then(
 			function(success) {
 				$scope.items = success.data;
@@ -33,4 +34,28 @@ app.controller(
 					$scope.items.splice(index, 1);
 				})
 		}
+		$scope.editItem = function(item){
+			var edit = {
+				body: item.body
+			}
+			$http.put('http://localhost:3000/items/'+ item.id +'.json',{ item: edit }).then(
+				function(success){
+					item = success.data;
+				},
+				function(err){
+					console.log(err);
+				})
+		}
+		 $scope.doneItems = function(item){
+		 	var done = {
+		 		status: item.status
+		 	}
+		 	$http.put('http://localhost:3000/items/'+ item.id +'.json',{ item: done }).then(
+		 		function(success){
+		 			item = success.data;
+		 		},
+		 		function(err){
+		 			console.log(err);
+		 		});
+		 }
 	});
