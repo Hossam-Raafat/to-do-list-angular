c42959d3e36e81786e4805d4ab64b74ecfdbb186
Hossam-Raafat <juggiiboy@gmail.com>
2017-05-16 18:26:16 +0200

    Add registration form to app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-var app = angular.module('todo-app', []);
+var app = angular.module('todo-app', ['ng-token-auth']);
 
 app.controller(
-	'todo-controller', function($scope, $http){
+	'todo-controller', function( $scope, $http, $auth){
 		$scope.toDo = 'To-Do';
 		$scope.done = 'Done';
 		$scope.items = [];
@@ -12,7 +12,7 @@ app.controller(
 			},
 			function(err) {
 				console.log(err);
-			})
+			});
 
 		$scope.addItem = function(){
 			var add = {
@@ -25,15 +25,15 @@ app.controller(
 				},
 				function(err){
 					console.log(err);
-				})
-		}
+				});
+		};
 		$scope.deleteItem = function(item){
 			$http.delete('http://localhost:3000/items/'+ item.id +'.json').then(
 				function(success){
 					var index = $scope.items.indexOf(item);
 					$scope.items.splice(index, 1);
-				})
-		}
+				});
+		};
 		$scope.editItem = function(item){
 			var edit = {
 				body: item.body
@@ -44,8 +44,8 @@ app.controller(
 				},
 				function(err){
 					console.log(err);
-				})
-		}
+				});
+		};
 		 $scope.doneItems = function(item){
 		 	var done = {
 		 		status: item.status
@@ -57,5 +57,39 @@ app.controller(
 		 		function(err){
 		 			console.log(err);
 		 		});
-		 }
-	});
+		 };
+		// $scope.registerUser = function(){
+		// 	$auth.submitRegistration($scope.registrationForm).then(
+		// 		function(success){
+
+		// 		}).catch(function(err){
+
+		// 		});
+		// };
+		$scope.handleRegBtnClick = function() {
+      $auth.submitRegistration($scope.registrationForm)
+        .then(function(resp) {
+          // handle success response
+          toaster.success('Successfuly register', 'good job!');
+        })
+        .catch(function(resp) {
+          // handle error response
+          toaster.error('Something went wrong', 'please try again!');
+        });
+      }
+
+		$scope.authenticationProcess = function(){
+			$auth.submitLogin($scope.loginForm).then(function(user){
+				toaster.success('Successful login', 'Bravo!');
+			}).catch(function(err){
+				toaster.error('Your credintials are wrong', 'Error');
+			});
+		};
+	}
+);
+app.config(function($authProvider) {
+    $authProvider.configure({
+      apiUrl: 'http://localhost:3000',
+      validateOnPageLoad: false
+		});
+});
